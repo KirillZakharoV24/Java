bc8e8ad1ad3291b09c380366986d25ddb5af793c
KirillZakharoV24 <156929709+KirillZakharoV24@users.noreply.github.com>
2024-05-14 20:07:17 +0500

    Update DZ2a.java

--- src/DZ2a.java
+++ src/DZ2a.java
@@ -4,10 +4,16 @@
 public class DZ2a {
     public static void main(String[] args) {
         // Стоимость билета
-        double ticketPrice = 15685;
+        int ticketPrice = 15685;
 
         // Количество начисленных миль
         int milesEarned =  (int) (ticketPrice / 20);
+
+        if (ticketPrice >= 20) {
+            milesEarned = ticketPrice / 20;
+        } else { milesEarned =0;
+            
+        }
 
         // Результат
         System.out.println("За билет стоимостью " + ticketPrice + " рублей начислено " + milesEarned + " миль.");
